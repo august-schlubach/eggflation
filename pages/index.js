7e04ea7fdb7d54076c6b852601caa04f4c7e0c7e
august schlubach <august.schlubach@gmail.com>
2025-05-03 14:32:59 -0400

    visual updates

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,10 @@ export default function Home() {
     <div className="d-flex justify-content-center align-items-center vh-100">
       <div className="center-box">
         <div className="text-center w-100">
-          <h2>The Price of Eggs</h2>
-          <p>March: $6.23</p>
-
-          <EggsChart />
           <p>
-            <Link href="/info">More Info</Link>
+            Eggs, Grade A, large, per doz. <em>March: $6.23</em>
           </p>
+          <EggsChart />
         </div>
       </div>
     </div>
