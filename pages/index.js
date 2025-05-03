b04c5ee87cf5861a58484f6b91d0b72f1727174b
august schlubach <august.schlubach@gmail.com>
2025-05-03 16:09:26 -0400

    added prose desc

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,17 @@ export default function Home() {
             <span className="detail">Grade A, large, per doz.</span>
           </p>
           <PriceChart csvUrl="/eggs-avg.csv" />
+          <div className="prose mt-4">
+            Egg prices are a useful indicator of consumer pressures because eggs
+            are a staple in nearly every household, making their cost highly
+            visible. Recent sharp increases, driven by inflation and supply
+            disruptions like avian flu, signal rising basic living expenses that
+            may force consumers to adjust spending or eating habits. Since eggs
+            are affordable and widely bought, price changes reflect broader
+            economic forces-such as supply chain issues and production
+            costs-making them a practical gauge of inflation’s impact on daily
+            life.
+          </div>
         </div>
       </div>
 
@@ -21,6 +32,19 @@ export default function Home() {
             <span className="detail">U.S. City Average</span>
           </p>
           <PriceChart csvUrl="/cpi.csv" />
+          <div className="prose mt-4">
+            <p>
+              The Consumer Price Index (CPI), calculated monthly by the U.S.
+              Bureau of Labor Statistics, measures the average change over time
+              in prices paid by consumers for a representative basket of goods
+              and services, such as food, housing, transportation, and
+              healthcare. As the most widely used indicator of inflation, the
+              CPI tracks shifts in the cost of living, guiding policymakers,
+              businesses, and consumers in adjusting wages, benefits, and
+              economic decisions; when the CPI rises, it signals higher living
+              costs and reduced purchasing power.
+            </p>
+          </div>
         </div>
       </div>
     </div>
