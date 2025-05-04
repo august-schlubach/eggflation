40b26e5954935be1674849cab7d2c1c1b601cfd3
august schlubach <august.schlubach@gmail.com>
2025-05-04 15:03:32 -0400

    adding google tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../public/styles.css";
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Add Google Analytics script to the head dynamically
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-K5HGPSCE4R";
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      // Initialize Google Analytics
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-K5HGPSCE4R");
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
